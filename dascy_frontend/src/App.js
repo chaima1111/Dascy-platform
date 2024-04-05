ccf56f14fe9928d1fe85e000607903021f16a75b
yasminezegaoui <yasminezegaoui1@gmail.com>
2024-04-05 20:50:54 +0100

    Modification of contact and search

--- dascy_frontend/src/App.js
+++ dascy_frontend/src/App.js
@@ -1,10 +1,6 @@
 import React from 'react'
 import {Navbar} from './components';
-<<<<<<< HEAD
-import {Courses,Header, About, Search,Contact} from './container';
-=======
-import {Courses,Header, About, Search,Feedbacks} from './container';
->>>>>>> 4d83dade33760b800bfff3dcbf4558a260b454fe
+import {Courses,Header, About, Search,Feedbacks,Contact} from './container';
 
 import './App.css';
 const App = () => {
@@ -15,11 +11,8 @@ const App = () => {
       <About/>
       <Courses/>
       <Search/>
-<<<<<<< HEAD
-      <Contact/>
-=======
       <Feedbacks/>
->>>>>>> 4d83dade33760b800bfff3dcbf4558a260b454fe
+      <Contact/>
     </div>
   )
 }
