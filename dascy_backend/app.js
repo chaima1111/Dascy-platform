9e5f283753675541be43356bb1ab472af6b55eba
SoumayaTgn <soumayatgnh@gmail.com>
2024-04-17 19:10:38 +0100

    users modification

--- dascy_backend/app.js
+++ dascy_backend/app.js
@@ -7,7 +7,6 @@ const app = express();
 app.use('/', require('./routes/index.js'));
  app.use('/users', require('./routes/users.js'));
 
- app.get
 app.listen(5004, () => {
     console.log('server is listening on port 5004....')
   })
--- dascy_backend/routes/users.js
+++ dascy_backend/routes/users.js
@@ -1,47 +1,32 @@
+const express = require('express');
+const router = express.Router();
+const { registerUser } = require('./project_db/database');// added 
 
-
-const express = require('express')
-const router = express.Router()
-
-
-//login Page
-router.get('/login',  (req, res) => res.render('Login'));
+// Login Page
+router.get('/login', (req, res) => res.render('Login'));
 
 // Register Page
-router.get('/register',  (req, res) => res.render('Register'));
+router.get('/register', (req, res) => res.render('Register'));
 
 // Register
 router.post('/register', (req, res) => {
-    const { name, yearOfStudy, university, password,password2, email } = req.body;
+    const { name, yearOfStudy, university, password, password2, email } = req.body;
     let errors = [];
-  
+
     if (!name || !yearOfStudy || !university || !email || !password || !password2) {
-      errors.push({ msg: 'Please enter all fields' });
+        errors.push({ msg: 'Please enter all fields' });
     }
-  
+
     if (password != password2) {
-      errors.push({ msg: 'Passwords do not match' });
+        errors.push({ msg: 'Passwords do not match' });
     }
-  
+
     if (password.length < 6) {
-      errors.push({ msg: 'Password must be at least 6 characters' });
+        errors.push({ msg: 'Password must be at least 6 characters' });
     }
-  
+
     if (errors.length > 0) {
-      res.render('register', {
-        errors,
-        name,
-        yearOfStudy,
-        university,
-        email,
-        password,
-        password2
-      });
-    } else {
-      User.findOne({ email: email }).then(user => {
-        if (user) {
-          errors.push({ msg: 'Email already exists' });
-          res.render('register', {
+        res.render('register', {
             errors,
             name,
             yearOfStudy,
@@ -49,50 +34,60 @@ router.post('/register', (req, res) => {
             email,
             password,
             password2
-          });
+        });
+    } else {
+      // user.finOne function removed from the else block
+
+      //here is the removed code 
+
+      /*User.findOne({ email: email }).then(user => {
+        if (user) {
+          errors.push({ msg: 'Email already exists' });
+          res.render('register', { errors, name, yearOfStudy, university, email, password, password2 });
         } else {
-          const newUser = new User({
-            name,
-            yearOfStudy,
-            university,
-            email,
-            password
-          });
-  
+          const newUser = new User({ name, yearOfStudy, university, email, password });
+
           bcrypt.genSalt(10, (err, salt) => {
             bcrypt.hash(newUser.password, salt, (err, hash) => {
               if (err) throw err;
               newUser.password = hash;
-              newUser
-                .save()
-                .then(user => {
-                  req.flash(
-                    'success_msg',
-                    'You are now registered and can log in'
-                  );
-                  res.redirect('/users/login');
-                })
-                .catch(err => console.log(err));
+              newUser.save().then(user => {
+                req.flash('success_msg', 'You are now registered and can log in');
+                res.redirect('/users/login');
+              }).catch(err => console.log(err));
             });
           });
         }
       });
     }
-  });
-  
+});*/
+        //here is the new code 
+        registerUser(name, yearOfStudy, university, email, password, (err, result) => {
+          if (err) {
+              // Handle error
+              return res.status(500).send('Internal Server Error');
+          }
+          req.flash('success_msg', 'You are now registered and can log in');
+          res.redirect('/users/login');
+        });
+        //end of new code
+  }
+});
+
 // Login
 router.post('/login', (req, res, next) => {
-    passport.authenticate('local', {
+  passport.authenticate('local', {
       successRedirect: '/dashboard',
       failureRedirect: '/users/login',
       failureFlash: true
-    })(req, res, next);
-  });
-  
-  // Logout
-  router.get('/logout', (req, res) => {
-    req.logout();
+  })(req, res, next);
+});
+
+// Logout
+router.get('/logout', (req, res) => {
+    req.logout(); 
     req.flash('success_msg', 'You are logged out');
     res.redirect('/users/login');
-  });
+});
+
 module.exports = router;
