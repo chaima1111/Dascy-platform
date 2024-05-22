57437eea7053340d9dae28145a92c06c99642e3a
bbasslaa <yasminezegaoui3@gmail.com>
2024-05-22 22:42:02 +0100

    graphs modification

--- dascy_frontend/src/Pages/PhyLayer/idTwo/IdTwo.jsx
+++ dascy_frontend/src/Pages/PhyLayer/idTwo/IdTwo.jsx
@@ -1,15 +1,13 @@
 import React from 'react'
 import { useState } from 'react';
 import './idTwo.css';
-
-const IdTwo = () => {
 /*<div className='dascy__idTwo-graph'>
               <div className="arrows">
                 <div className="arrow-x"></div>
                 <div className="arrow-y"></div>
               </div>
               </div> 
-<div className='dascy__idTwo-icon'>1</div>
+<div>1</div>
               <div className='dascy__idTwo-description'>
                 <h4>Codage tout ou rien (unipolaire)</h4>
               </div>
@@ -19,7 +17,7 @@ const IdTwo = () => {
               <p>On utlise deux valeurs pour representer le 0 et le 1</p>
             </div>
 <div className='dascy__idTwo-icon'>3</div>
-            <div className='dascy__idTwo-description'>
+            <div>
               <h4>Codage NRZ (No Return to Zero)</h4>
               <p>On utlise deux valeurs pour representer le 0 et le 1</p>
             </div>
@@ -28,23 +26,12 @@ const IdTwo = () => {
               <h4>Codage RZ (Return to Zero)</h4>
               <p>On utlise deux valeurs pour representer le 0 et le 1</p>
             </div>
-
-
-<div className="lines2">
-                    <div className='graph2_Line-1'></div>
-                    <div className='graph2_Line-2'></div>
-                    <div className='graph2_Line-3'></div>
-                    <div className='graph2_Line-4'></div>
-                    <div className='graph2_Line-5'></div>
-                    <div className='graph2_Line-6'></div>
-                    <div className='graph2_Line-7'></div>
-                    <div className='graph2_Line-8'></div>
-                    <div className='graph2_Line-9'></div>
-                    <div className='graph2_Line-10'></div>
-                    <div className='graph2_Line-11'></div>
-                    <div className='graph2_Line-12'></div>
-                  </div>
 */
+/*const [selectedCard, setSelectedCard] = useState('c1'); */
+const IdTwo = () => {
+
+  const [selectedCard, setSelectedCard] = useState('c1');
+  
 const [hoverMessage, setHoverMessage] = useState('');
 
 const handleMouseEnter = (message) => {
@@ -74,14 +61,25 @@ const [hoverMessage2, setHoverMessage2] = useState('');
   const handleMouseLeave3 = () => {
     setHoverMessage3('');
   };
+  const [hoverMessage4, setHoverMessage4] = useState('');
+
+const handleMouseEnter4 = (message) => {
+  setHoverMessage4(message);
+};
+
+const handleMouseLeave4 = () => {
+  setHoverMessage4('');
+};
   return (
     <>
     <div className='dascy__idTwo'>
       <div className='dascy__idTwo-container'>
-          <input type="radio" name='slide' id='c1' checked />
+          <input type="radio" name='slide' id='c1' checked={selectedCard === 'c1'}
+          onChange={() => setSelectedCard('c1')} />
           <label htmlFor="c1" className='dascy__idTwo-card' >
             <div className='dascy__idTwo-row'>
-            <div className='dascy__idTwo-graph'>
+            <p className='dascy__idTwo-title'>Tension</p>
+            <div className='dascy__idTwo-graph'>  
               <div className="arrows">
                 <div className="arrow-x" id='aar1-x'></div>
                 <div className="arrow-y" id='arr1-y'></div>
@@ -96,17 +94,37 @@ const [hoverMessage2, setHoverMessage2] = useState('');
                 <div className="dashed-line6 dash-line6" ></div>
               </div>
               <div className="lines">
-                <div className="line" id='LN'></div>
+                <div 
+                  className="line" id='LN'
+                  onMouseEnter={() => handleMouseEnter('un tension nulle code le 0')}
+                  onMouseLeave={handleMouseLeave}
+                ></div>
                 <div className="line1" id='LN1'></div>
-                <div className="line2" id='LN2'></div>
+                <div 
+                  className="line2" id='LN2'
+                  onMouseEnter={() => handleMouseEnter('une tension positive code le 1')}
+                  onMouseLeave={handleMouseLeave}
+                ></div>
                 <div className="line3" id='LN3'></div>
                 <div className="line4" id='LN4'></div>
-                <div className="line5" id='LN5'></div>
+                <div 
+                  className="line5" id='LN5'
+                  onMouseEnter={() => handleMouseEnter('un tension nulle code le 0')}
+                  onMouseLeave={handleMouseLeave}
+                ></div>
                 <div className="line6" id='LN6'></div>
                 <div className="line7" id='LN7'></div>
-                <div className="line8" id='LN8'></div>
+                <div 
+                  className="line8" id='LN8'
+                  onMouseEnter={() => handleMouseEnter('une tension positive code le 1')}
+                  onMouseLeave={handleMouseLeave}
+                ></div>
                 <div className="line9" id='LN9'></div>
-                <div className="line10" id='LN10'></div>
+                <div 
+                  className="line10" id='LN10'
+                  onMouseEnter={() => handleMouseEnter('un tension nulle code le 0')}
+                  onMouseLeave={handleMouseLeave}
+                ></div>
               </div>
               <div className='nmbrs'>
                 <p>0</p>
@@ -116,13 +134,24 @@ const [hoverMessage2, setHoverMessage2] = useState('');
                 <p>0</p>
                 <p>1</p>
                 <p>0</p>
+                <p className='temps'>Temps</p>
               </div> 
+              {hoverMessage && <div className="hover-message">{hoverMessage}</div>}
+              <div className='icon-description'>
+                <div className='icon'>1</div>
+                <div className='description'>
+                  <h4>Codage tout ou rien (unipolaire)</h4>
+                  <p>On utlise deux valeurs pour representer le 0 et le 1</p>
+                </div>
+              </div>
               </div>
             </div>
           </label>
-          <input type="radio" name='slide' id='c2' checked/>
+          <input type="radio" name='slide' id='c2' checked={selectedCard === 'c2'}
+          onChange={() => setSelectedCard('c2')}/>
         <label htmlFor="c2" className='dascy__idTwo-card' >
           <div className='dascy__idTwo-row'>
+          <p className='tension'>Tension</p>
               <div className='dascy__idTwo-graph'>
                   <div className="arrows2">
                     <div className="arrow-x" id='arr2-x'></div>
@@ -140,39 +169,39 @@ const [hoverMessage2, setHoverMessage2] = useState('');
                   <div className="lines2">
               <div
                 className='graph2_Line-1'
-                onMouseEnter={() => handleMouseEnter('un tension nulle code le 0')}
-                onMouseLeave={handleMouseLeave}
+                onMouseEnter={() => handleMouseEnter4('un tension nulle code le 0')}
+                onMouseLeave={handleMouseLeave4}
               ></div>
               <div className='graph2_Line-2'></div>
               <div
                 className='graph2_Line-3'
-                onMouseEnter={() => handleMouseEnter('une tension positive indique le 1')}
-                onMouseLeave={handleMouseLeave}
+                onMouseEnter={() => handleMouseEnter4('une tension positive indique le 1')}
+                onMouseLeave={handleMouseLeave4}
               ></div>
               <div className='graph2_Line-4'></div>
               <div
                 className='graph2_Line-5'
-                onMouseEnter={() => handleMouseEnter('une tension negative indique le 1')}
-                onMouseLeave={handleMouseLeave}
+                onMouseEnter={() => handleMouseEnter4('une tension negative indique le 1')}
+                onMouseLeave={handleMouseLeave4}
               ></div>
               <div className='graph2_Line-6'></div>
               <div className='graph2_Line-7'></div>
               <div 
                 className='graph2_Line-8'
-                onMouseEnter={() => handleMouseEnter('un tension nulle code le 0')}
-                onMouseLeave={handleMouseLeave}
+                onMouseEnter={() => handleMouseEnter4('un tension nulle code le 0')}
+                onMouseLeave={handleMouseLeave4}
               ></div>
               <div className='graph2_Line-9'></div>
               <div 
                 className='graph2_Line-10'
-                onMouseEnter={() => handleMouseEnter('une tension positive indique le 1')}
-                onMouseLeave={handleMouseLeave}
+                onMouseEnter={() => handleMouseEnter4('une tension positive indique le 1')}
+                onMouseLeave={handleMouseLeave4}
               ></div>
               <div className='graph2_Line-11'></div>
               <div
                 className='graph2_Line-12'
-                onMouseEnter={() => handleMouseEnter('un tension nulle code le 0')}
-                onMouseLeave={handleMouseLeave}
+                onMouseEnter={() => handleMouseEnter4('un tension nulle code le 0')}
+                onMouseLeave={handleMouseLeave4}
               ></div>
             </div>
                   <div className='nmbrs' id='nbr'>
@@ -183,14 +212,24 @@ const [hoverMessage2, setHoverMessage2] = useState('');
                     <p>0</p>
                     <p>1</p>
                     <p>0</p>
+                    <p className='temps2'>Temps</p>
                   </div> 
-                  {hoverMessage && <div className="hover-message">{hoverMessage}</div>}
+                  {hoverMessage4 && <div className="hover-message4">{hoverMessage4}</div>}
+                  <div className='icon-description'>
+                    <div className='icon2'>2</div>
+                    <div className='description' id='dscrption'>
+                  <h4>Codage bipolaire</h4>
+                  <p>On utlise deux valeurs pour representer le 0 et le 1</p>
+                  </div>
+              </div>
               </div> 
           </div>
         </label>
-        <input type="radio" name='slide' id='c3' />
+        <input type="radio" name='slide' id='c3'  checked={selectedCard === 'c3'}
+          onChange={() => setSelectedCard('c3')}/>
         <label htmlFor="c3" className='dascy__idTwo-card' >
           <div className='dascy__idTwo-row'>
+          <p className='tension'>Tension</p>
             <div className='dascy__idTwo-graph'>
                   <div className="arrows3">
                     <div className="arrow-x" id='arr2-x'></div>
@@ -245,14 +284,24 @@ const [hoverMessage2, setHoverMessage2] = useState('');
                     <p>0</p>
                     <p>1</p>
                     <p>0</p>
+                    <p className='temps2'>Temps</p>
                   </div> 
                   {hoverMessage2 && <div className="hover-message2">{hoverMessage2}</div>}
+                  <div className='icon-description'>
+                    <div className='icon3'>3</div>
+                    <div className='description' id='dscrption3'>
+                  <h4>Codage NRZ (No Return to Zero)</h4>
+                  <p>On utlise deux valeurs pour representer le 0 et le 1</p>
+                  </div>
+              </div>
             </div> 
           </div>
         </label>
-        <input type="radio" name='slide' id='c4' />
+        <input type="radio" name='slide' id='c4'  checked={selectedCard === 'c4'}
+          onChange={() => setSelectedCard('c4')}/>
         <label htmlFor="c4" className='dascy__idTwo-card' >
           <div className='dascy__idTwo-row'>
+          <p className='dascy__idTwo-title4'>Tension</p>
           <div className='dascy__idTwo-graph'>
               <div className="arrows4">
                 <div className="arrow-x" id='aar1-x'></div>
@@ -269,26 +318,26 @@ const [hoverMessage2, setHoverMessage2] = useState('');
               </div>
               <div className="lines4">
               <div className="graph4_line-1"></div>
-              <div
-                className="graph4_line-2"
+              <div className="graph4_line-2"></div>
+              <div 
+                className="graph4_line-3"
                 onMouseEnter={() => handleMouseEnter3('Front descendant = 1')}
                 onMouseLeave={handleMouseLeave3}
               ></div>
-              <div className="graph4_line-3"></div>
               <div className="graph4_line-4"></div>
-              <div
-                className="graph4_line-5"
+              <div className="graph4_line-5"></div>
+              <div 
+                className="graph4_line-6"
                 onMouseEnter={() => handleMouseEnter3('Front descendant = 1')}
                 onMouseLeave={handleMouseLeave3}
               ></div>
-              <div className="graph4_line-6"></div>
               <div className="graph4_line-7"></div>
-              <div
-                className="graph4_line-8"
+              <div className="graph4_line-8"></div>
+              <div 
+                className="graph4_line-9"
                 onMouseEnter={() => handleMouseEnter3('Front descendant = 1')}
                 onMouseLeave={handleMouseLeave3}
               ></div>
-              <div className="graph4_line-9"></div>
             </div>
               <div className='nmbrs'>
                 <p>0</p>
@@ -298,14 +347,24 @@ const [hoverMessage2, setHoverMessage2] = useState('');
                 <p>0</p>
                 <p>1</p>
                 <p>0</p>
+                <p className='temps4'>Temps</p>
               </div> 
               {hoverMessage3 && <div className="hover-message3">{hoverMessage3}</div>}
+              <div className='icon-description'>
+                <div className='icon'>4</div>
+                <div className='description'>
+                  <h4>Codage RZ (Return to Zero)</h4>
+                  <p>On utlise deux valeurs pour representer le 0 et le 1</p>
+                </div>
+              </div>
               </div>
           </div>
         </label>
-        <input type="radio" name='slide' id='c5' />
+        <input type="radio" name='slide' id='c5'  checked={selectedCard === 'c5'}
+          onChange={() => setSelectedCard('c5')}/>
         <label htmlFor="c5" className='dascy__idTwo-card' >
           <div className='dascy__idTwo-row'>
+          <p className='tension'>Tension</p>
           <div className='dascy__idTwo-graph'>
                   <div className="arrows5">
                     <div className="arrow-x" id='arr2-x'></div>
@@ -348,13 +407,23 @@ const [hoverMessage2, setHoverMessage2] = useState('');
                     <p>0</p>
                     <p>1</p>
                     <p>0</p>
+                    <p className='temps2'>Temps</p>
                   </div> 
+                  <div className='icon-description'>
+                <div className='icon5'>5</div>
+                <div className='description'>
+                  <h4>Codage Manchester différentiel</h4>
+                  <p>On utlise deux valeurs pour representer le 0 et le 1</p>
+                </div>
+              </div>
             </div> 
           </div>
         </label>
-        <input type="radio" name='slide' id='c6' />
+        <input type="radio" name='slide' id='c6'  checked={selectedCard === 'c6'}
+          onChange={() => setSelectedCard('c6')}/>
         <label htmlFor="c6" className='dascy__idTwo-card' >
           <div className='dascy__idTwo-row'>
+          <p className='tension'>Tension</p>
           <div className='dascy__idTwo-graph'>
                   <div className="arrows6">
                     <div className="arrow-x" id='arr2-x'></div>
@@ -392,7 +461,15 @@ const [hoverMessage2, setHoverMessage2] = useState('');
                     <p>0</p>
                     <p>1</p>
                     <p>0</p>
+                    <p className='temps2'>Temps</p>
                   </div> 
+                  <div className='icon-description'>
+                <div className='icon6'>6</div>
+                <div className='description'>
+                  <h4>Codage Manchester différentiel</h4>
+                  <p>On utlise deux valeurs pour representer le 0 et le 1</p>
+                </div>
+              </div>
             </div> 
           </div>
         </label>
